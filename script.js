def29dbc81b858a055b981ef1d283cbb66db1dcd
Jason Slabbers <jslabbers@gmail.com>
2019-07-08 15:02:03 +0200

    6th

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function makeResizableDiv(){
-    alert('bla')
+    alert('Bla')
     const wrapper = document.querySelector('.wrapper');
     let resizeableDiv = document.querySelectorAll('div .item');
     let minimumSize = 50;
@@ -10,40 +10,40 @@ function makeResizableDiv(){
 
     for (let i = 0; i < resizeableDiv.length; i++) {
         const currentDiv = resizeableDiv[i];
-        // currentDiv.addEventListener('mousedown', function(e){
+        currentDiv.addEventListener('mousedown', function(e){
             
-        //     e.preventDefault();
-        //     originalWidth = parseFloat(getComputedStyle(currentDiv, null).getPropertyValue('width').replace('px', ''));
-        //     originalX = currentDiv.getBoundingClientRect().right;
-        //     originalMouseX = e.pageX;
+            e.preventDefault();
+            originalWidth = parseFloat(getComputedStyle(currentDiv, null).getPropertyValue('width').replace('px', ''));
+            originalX = currentDiv.getBoundingClientRect().right;
+            originalMouseX = e.pageX;
 
-        //     wrapper.addEventListener('mousemove', resize);
-        //     window.addEventListener('mouseup', stopResize);
+            wrapper.addEventListener('mousemove', resize);
+            window.addEventListener('mouseup', stopResize);
 
-        //     function resize(e){
-        //         if (currentDiv) {
-        //             const width = originalWidth + (e.pageX - originalMouseX);
+            function resize(e){
+                if (currentDiv) {
+                    const width = originalWidth + (e.pageX - originalMouseX);
 
-        //             if(width > minimumSize){
-        //                 currentDiv.style.width = width + 'px';
-        //                 document.getElementById('col1').innerText = width + 'px';
-        //             }
-        //         }
-        //     }
-        //     function stopResize() {
-        //         wrapper.removeEventListener('mousemove', resize);
-        //     }
-        // });
+                    if(width > minimumSize){
+                        currentDiv.style.width = width + 'px';
+                        document.getElementById('col1').innerText = width + 'px';
+                    }
+                }
+            }
+            function stopResize() {
+                wrapper.removeEventListener('mousemove', resize);
+            }
+        });
 
-        currentDiv.addEventListener('ontouchstart', function(e) {
+        currentDiv.addEventListener('touchstart', function(e) {
             alert(e);
             // e.preventDefault();
             originalWidth = parseFloat(getComputedStyle(currentDiv, null).getPropertyValue('width').replace('px', ''));
             originalX = currentDiv.getBoundingClientRect().right;
             originalMouseX = e.pageX;
 
-            wrapper.addEventListener('ontouchmove', resize);
-            wrapper.addEventListener('ontouchend', stopResize);
+            wrapper.addEventListener('touchmove', resize);
+            wrapper.addEventListener('touchend', stopResize);
 
             function resize(e){
                 if (currentDiv) {
