e2b729bdbaef74896b2da9b196ddb55561a2ff4a
Jason Slabbers <jslabbers@gmail.com>
2019-07-08 16:27:17 +0200

    22

--- script.js
+++ script.js
@@ -34,42 +34,39 @@ function makeResizableDiv(){
         });
     }
 }
+function makeResizableDivTouch(){
+    const wrapper = document.querySelector('.wrapper');
+    let resizeableDiv = document.querySelectorAll('div .item');
+    let minimumSize = 50;
 
-window.addEventListener('DOMContentLoaded', (e) => {
-    function makeResizableDivTouch(){
-        const wrapper = document.querySelector('.wrapper');
-        let resizeableDiv = document.querySelectorAll('div .item');
-        let minimumSize = 50;
-    
-        let originalWidth = 0;
-        let originalX = 0;
-        let originalMouseX = 0;
-    
-        for (let i = 0; i < resizeableDiv.length; i++) {
-            const currentDiv = resizeableDiv[i];
-            currentDiv.addEventListener('touchstart', (e) => {
-                e.preventDefault();
-                originalWidth = parseFloat(getComputedStyle(currentDiv, null).getPropertyValue('width').replace('px', ''));
-                originalX = currentDiv.getBoundingClientRect().right;
-                originalMouseX = e.pageX;
-    
-                wrapper.addEventListener('touchmove', resize);
-                wrapper.addEventListener('touchend', stopResize);
-    
-                function resize(e){
-                    if (currentDiv) {
-                        const width = originalWidth + (e.pageX - originalMouseX);
-    
-                        if(width > minimumSize){
-                            currentDiv.style.width = width + 'px';
-                            document.getElementById('col1').innerText = width + 'px';
-                        }
+    let originalWidth = 0;
+    let originalX = 0;
+    let originalMouseX = 0;
+
+    for (let i = 0; i < resizeableDiv.length; i++) {
+        const currentDiv = resizeableDiv[i];
+        currentDiv.addEventListener('touchstart', (e) => {
+            e.preventDefault();
+            originalWidth = parseFloat(getComputedStyle(currentDiv, null).getPropertyValue('width').replace('px', ''));
+            originalX = currentDiv.getBoundingClientRect().right;
+            originalMouseX = e.pageX;
+
+            wrapper.addEventListener('touchmove', resize);
+            wrapper.addEventListener('touchend', stopResize);
+
+            function resize(e){
+                if (currentDiv) {
+                    const width = originalWidth + (e.pageX - originalMouseX);
+
+                    if(width > minimumSize){
+                        currentDiv.style.width = width + 'px';
+                        document.getElementById('col1').innerText = width + 'px';
                     }
                 }
-                function stopResize() {
-                    wrapper.removeEventListener('touchmove', resize);
-                }
-            });
-        }
+            }
+            function stopResize() {
+                wrapper.removeEventListener('touchmove', resize);
+            }
+        });
     }
-});
+}
