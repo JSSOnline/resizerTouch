25a13be8ead7ee64b637f4bd257a3b79c0f2ae52
Jason Slabbers <jslabbers@gmail.com>
2019-07-09 10:57:09 +0200

    49

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ function makeResizableDivTouch(){
             function resize(e){
                 if (currentDiv) {
                     const width = originalWidth + (e.pageX - originalMouseX);
-                    alert(e)
+                    alert(e.changedTouches)
                     if(width > minimumSize){
                         currentDiv.style.width = width + 'px';
                         document.getElementById('col1').innerText = width + 'px';
